702eb554b301455c57ca5ce8c65143f72e436320
shadow <lightshadow416@gmail.com>
2024-03-26 23:30:28 +0100

    fixing import links

--- src/array.ts
+++ src/array.ts
@@ -1,10 +1,10 @@
 import { dishes_t } from "./types";
-import img1 from "../public/algerian-food-chorba-frik.jpg";
-import img2 from "../public/hrira.jpg";
-import img3 from "../public/Algerian_Chakhchoukha.jpg";
-import img4 from "../public/Bean_soup_with_tomatoes_and_red_peppers.jpeg";
-import img5 from "../public/cosocs.jpg";
-import img6 from "../public/ma9rot.jpg";
+import img1 from "/algerian-food-chorba-frik.jpg";
+import img2 from "/hrira.jpg";
+import img3 from "/Algerian_Chakhchoukha.jpg";
+import img4 from "/Bean_soup_with_tomatoes_and_red_peppers.jpeg";
+import img5 from "/cosocs.jpg";
+import img6 from "/ma9rot.jpg";
 export const dishes: dishes_t[] = [
   {
     title: "chorba-frik",
